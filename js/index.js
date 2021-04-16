6f789cc37fabb7a86412739b42a0e752c44b319d
José Manuel Aliaga <jose.man.aliaga@dxc.com>
2021-04-16 11:04:39 -0300

    hiddne and shown text as expected

--- js/index.js
+++ js/index.js
@@ -1,23 +1,28 @@
-$( function () {
-    $('#derecha').show();
-    var trigger = $('#hamburger'),
-        isClosed = true;
+if (window.outerWidth < 600) {
+    $(function () {
+        let trigger = $('#hamburger');
+        let isClosed = true;
+        
+        trigger.on("load", function() {
+            burgerTime();
+        })
 
-    trigger.on("click",function () {
-        burgerTime();
-    });
-
-    function burgerTime() {
-        if (isClosed == true) {
-            trigger.removeClass('is-open');
-            trigger.addClass('is-closed');
-            isClosed = false;
-            $('#derecha').hide();
-        } else {
-            trigger.removeClass('is-closed');
-            trigger.addClass('is-open');
-            isClosed = true;
-            $('#derecha').show();
+        trigger.on("click", function () {
+            burgerTime();
+        });
+        
+        function burgerTime() {
+            if (isClosed == true) {
+                trigger.removeClass('is-open');
+                trigger.addClass('is-closed');
+                isClosed = false;
+                $('#derecha').toggle();
+            } else {
+                trigger.removeClass('is-closed');
+                trigger.addClass('is-open');
+                isClosed = true;
+                $('#derecha').toggle();
+            }
         }
-    }
-})
+    })
+}
